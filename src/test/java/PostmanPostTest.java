b403685cde2f21077499564b5218aa453e919544
Pavel Kozlov <cozlov.pavel@mail.ru>
2023-03-10 22:02:49 +0300

    Add test

---
+++ src/test/java/PostmanPostTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanPostTest {
+    @Test
+    void shouldPostRequest(){
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Test data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("Test data"))
+        ;
+    }
+}
